ef9fc9db4f787ada021840c707551096a570232d
Sean Teramae <seteramae@gmail.com>
2021-10-10 21:30:26 -0700

    Add build to pipeline

--- assets/src/components/index.js
+++
@@ -1,8 +0,0 @@
-import '../styles/tokens.css.js';
-import '../styles/text/text.css.js';
-export { default as StButton } from './button/Button.js';
-export { default as StCard } from './card/Card.js';
-export { default as StFooter } from './footer/Footer.js';
-export { default as StHeader } from './header/Header.js';
-export { default as StNav } from './navigation/Navigation.js';
-export { default as StSection } from './section/Section.js';
--- assets/src/styles/text/text.css.js
+++
@@ -1,6 +0,0 @@
-import styleInject from '../../../node_modules/style-inject/dist/style-inject.es.js';
-
-var css_248z = "h1,\nh2,\nh3,\nh4,\nh5,\nh6,\np,\na {\n  margin: 0;\n}\na {\n  text-decoration: none;\n}\n.st-text,\n.st-text::slotted(*) {\n  font-family: var(--font-family-default), sans-serif;\n  font-size: var(--font-size-var, var(--font-size-100));\n  line-height: 1.6;\n}\n.st-text-accent,\n.st-text-accent::slotted(*) {\n  font-family: var(--font-family-accent), sans-serif;\n  font-size: var(--font-size-var, var(--font-size-500));\n  line-height: 1.2;\n}\n.st-text-100,\n.st-text-100::slotted(*) {\n  --font-size-var: var(--font-size-100);\n}\n.st-text-200,\n.st-text-200::slotted(*) {\n  --font-size-var: var(--font-size-200);\n}\n.st-text-300,\n.st-text-300::slotted(*) {\n  --font-size-var: var(--font-size-300);\n}\n.st-text-400,\n.st-text-400::slotted(*) {\n  --font-size-var: var(--font-size-400);\n}\n.st-text-500,\n.st-text-500::slotted(*) {\n  --font-size-var: var(--font-size-500);\n}\n.st-text-600,\n.st-text-600::slotted(*) {\n  --font-size-var: var(--font-size-600);\n}\n.st-text-700,\n.st-text-700::slotted(*) {\n  --font-size-var: var(--font-size-700);\n}\n.st-text-800,\n.st-text-800::slotted(*) {\n  --font-size-var: var(--font-size-800);\n}\n";
-styleInject(css_248z);
-
-export default css_248z;
